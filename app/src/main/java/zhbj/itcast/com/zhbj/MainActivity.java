1a7c5fc49d0af820b59c12670253ba73b8bbf6c2
tangpj <tangpj@samples.cn>
2018-09-04 17:03:59 +0800

    侧边栏module追加，并使用实例 参考文章：https://blog.csdn.net/niubitianping/article/details/52218537，方法2 至Day01.视频11 完成

--- app/src/main/java/zhbj/itcast/com/zhbj/MainActivity.java
+++ app/src/main/java/zhbj/itcast/com/zhbj/MainActivity.java
@@ -1,13 +1,25 @@
 package zhbj.itcast.com.zhbj;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingActivity;
+
+public class MainActivity extends SlidingActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //设置侧边栏的布局
+        setBehindContentView(R.layout.left_menu);
+
+        //获取当前侧边栏布局对象
+        SlidingMenu slidingMenu = getSlidingMenu();
+        //设置触摸模式为全屏模式
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        //设置侧边栏宽度
+        slidingMenu.setBehindOffset(900);   //屏幕预留900px的宽度
     }
 }
