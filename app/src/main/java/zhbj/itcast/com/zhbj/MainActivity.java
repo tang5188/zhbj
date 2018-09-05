b63deb687ee7f400c94a462156bc13cc502858ca
tangpj <tangpj@samples.cn>
2018-09-05 17:15:14 +0800

    将布局替换未fragment 至Day01.视频14 完成

--- app/src/main/java/zhbj/itcast/com/zhbj/MainActivity.java
+++ app/src/main/java/zhbj/itcast/com/zhbj/MainActivity.java
@@ -1,11 +1,29 @@
 package zhbj.itcast.com.zhbj;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.jeremyfeinstein.slidingmenu.lib.app.SlidingActivity;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
-public class MainActivity extends SlidingActivity {
+import zhbj.itcast.com.zhbj.fragment.ContentFragment;
+import zhbj.itcast.com.zhbj.fragment.LeftMenuFragment;
+
+/**
+ * 主页面
+ * 1.当一个activity要展示fragment时，必须继承FragmentActivity
+ * 2.使用FragmentManager进行布局替换
+ * 3.将现有布局掏空，根布局建议使用FrameLayout
+ * 4.开始事务，进行替换操作，并提交
+ */
+public class MainActivity extends SlidingFragmentActivity {
+
+    private static final String TAG_CONTENT = "TAG_CONTENT";
+    private static final String TAG_LEFT_MENU = "TAG_LEFT_MENU";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,5 +39,22 @@ public class MainActivity extends SlidingActivity {
         slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
         //设置侧边栏宽度
         slidingMenu.setBehindOffset(900);   //屏幕预留900px的宽度
+
+        initFragment();
+    }
+
+    /**
+     * 初始化fragment
+     */
+    private void initFragment() {
+        //获取fragment的管理器
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction(); //开启一个事务
+        transaction.replace(R.id.fl_content, new ContentFragment(), TAG_CONTENT);  //使用fragment替换现有布局，参1：当前布局id，参2：要替换的布局
+        transaction.replace(R.id.fl_left_menu, new LeftMenuFragment(), TAG_LEFT_MENU);  //TAG标记，方便以后找到对象
+        transaction.commit();       //提交事务
+
+        //通过此方法，查找fragment对象
+        //ContentFragment fragment = (ContentFragment) fm.findFragmentByTag(TAG_CONTENT);
     }
 }
