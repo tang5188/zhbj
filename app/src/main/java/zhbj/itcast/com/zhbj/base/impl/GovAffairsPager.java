29db63b9d73d762ef95f60dfd7792febae04310a
tangpj <tangpj@samples.cn>
2018-09-07 19:23:26 +0800

    basePager子类的实现 至Day02.视频02 完成

---
+++ app/src/main/java/zhbj/itcast/com/zhbj/base/impl/GovAffairsPager.java
@@ -0,0 +1,28 @@
+package zhbj.itcast.com.zhbj.base.impl;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.widget.TextView;
+
+import zhbj.itcast.com.zhbj.base.BasePager;
+
+/**
+ * 政务
+ */
+public class GovAffairsPager extends BasePager {
+    public GovAffairsPager(Activity activity) {
+        super(activity);
+    }
+
+    @Override
+    public void InitData() {
+        //给空的帧布局添加布局对象
+        TextView view = new TextView(mActivity);
+        view.setTextSize(22);
+        view.setTextColor(Color.RED);
+        view.setGravity(Gravity.CENTER);    //居中显示
+        view.setText("政务");
+        flContainer.addView(view);      //给帧布局添加对象
+    }
+}
---
+++ app/src/main/java/zhbj/itcast/com/zhbj/base/impl/SettingPager.java
@@ -0,0 +1,28 @@
+package zhbj.itcast.com.zhbj.base.impl;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.Gravity;
+import android.widget.TextView;
+
+import zhbj.itcast.com.zhbj.base.BasePager;
+
+/**
+ * 设置
+ */
+public class SettingPager extends BasePager {
+    public SettingPager(Activity activity) {
+        super(activity);
+    }
+
+    @Override
+    public void InitData() {
+        //给空的帧布局添加布局对象
+        TextView view = new TextView(mActivity);
+        view.setTextSize(22);
+        view.setTextColor(Color.RED);
+        view.setGravity(Gravity.CENTER);    //居中显示
+        view.setText("设置");
+        flContainer.addView(view);      //给帧布局添加对象
+    }
+}
