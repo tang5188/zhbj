1dff25f7f8f80e05a7bee1b8b36eeb33756b4aed
tangpj <tangpj@samples.cn>
2018-11-24 14:44:21 +0800

    04.02.滑动显示下拉刷新.avi

--- app/src/main/java/zhbj/itcast/com/zhbj/view/RefreshListView.java
+++ app/src/main/java/zhbj/itcast/com/zhbj/view/RefreshListView.java
@@ -2,6 +2,7 @@ package zhbj.itcast.com.zhbj.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ListView;
 
@@ -39,5 +40,37 @@ public class RefreshListView extends ListView {
         mHeaderView.measure(0, 0);   //手动测量，传值为0，表示由系统决定测量
         this.measuredHeight = mHeaderView.getMeasuredHeight();
         mHeaderView.setPadding(0, -this.measuredHeight, 0, 0);
+    }
+
+    private int startY = -1;
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                startY = (int) ev.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                //没有获取到按下的事件（按住新闻头条滑动时，按下事件被viewPager消费了）
+                if (startY == -1) {
+                    startY = (int) ev.getY();
+                }
+                int endY = (int) ev.getY();
+                int dy = endY - startY;
+                int firstVisiblePosition = this.getFirstVisiblePosition();  //当前显示的第一个item的位置
+                if (dy > 0 && firstVisiblePosition == 0) {
+                    //下拉动作，并且当前的listview的顶部
+                    int padding = -this.measuredHeight + dy;
+                    mHeaderView.setPadding(0, padding, 0, 0);
+                    //消费此事件，处理下拉刷新控件的滑动，不需要listview原生效果参与
+                    return true;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            default:
+                break;
+        }
+        return super.onTouchEvent(ev);
     }
 }
